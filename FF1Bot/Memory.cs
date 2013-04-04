ec4f98f1a94e3b580f4ef2263a50a93fcb668a09
Kolpa <kolyaopahle@gmail.com>
2013-04-04 18:45:11 +0200

    memory related class

---
+++ FF1Bot/Memory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Timers;
+
+namespace FF1Bot
+{
+    public class Memory
+    {
+        #region User32.dll Importe
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out int lpdwProcessId);
+
+        [DllImport("user32.dll")]
+        private static extern bool GetWindowRect(IntPtr hWnd, out Rectangle rect);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+        
+        [DllImport("kernel32.dll")]
+        private static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, UIntPtr nSize, ref uint lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        private static extern bool CloseHandle(IntPtr hObject);
+
+        #endregion
+
+        private static IntPtr _handle;
+
+        public Memoy(int id)
+        {
+            _handle = OpenProcess(0x1F0FFF, true, id);
+        }
+    }
+}

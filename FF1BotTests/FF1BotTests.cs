77dbf7cd2356455dad205976d721eb951e2eef07
Deathmic <deathmic@gmail.com>
2013-04-04 18:45:15 +0200

    Grundlogik in eigene Klasse refaktorisiert, ersten Dummy-Test erstellt

---
+++ FF1BotTests/FF1BotTests.cs
@@ -0,0 +1,17 @@
+﻿using FF1Bot;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FF1BotTests
+{
+    [TestClass]
+    public class FF1BotTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Bot bot = new Bot();
+            Assert.IsNotNull(bot);
+            Assert.IsFalse(bot.ShutdownRequested);
+        }
+    }
+}
